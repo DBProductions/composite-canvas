2108723dbd5b59abdd04344e02d55d0e9f32672d
Dennis Braun <dennis@dbproductions.de>
2012-02-03 10:35:17 +0100

    add script file

---
+++ composite-canvas.js
@@ -0,0 +1,83 @@
+// basic properties
+var CompositeCanvas = function ()  {
+	this.canvasLayer = [];
+	this.elements = [];
+	this.width = 500;
+	this.height = 400;
+	this.color = '#fff';
+}
+// --------------------------------------------------------------------------------
+// get context
+CompositeCanvas.prototype.getCtx = function(canvas, color) {
+    var c = document.getElementById(canvas),
+		ctx = c.getContext('2d');
+	(undefined !== color) ? ctx.fillStyle = color : ctx.fillStyle = this.color; 	
+	return ctx;
+}	
+// --------------------------------------------------------------------------------
+// add a canvas
+CompositeCanvas.prototype.addLayer = function(canvas, fromBuffer) {
+	this.canvasLayer.push(canvas);				
+	var element = document.createElement('canvas');
+	element.setAttribute('id', canvas);			
+	element.setAttribute('width', this.width + 'px');
+	element.setAttribute('height', this.height + 'px');
+	document.getElementById('output').appendChild(element);				
+	this.elements.push(element);
+	// maybe needed
+	if (fromBuffer != undefined) {												
+		var curCanvas = document.getElementById( canvas ),
+			curCtx = curCanvas.getContext('2d'),
+			bufCanvas = document.getElementById('buffer'),
+			bufCtx = bufCanvas.getContext('2d');									
+		// draw buffer to canvas
+		curCtx.drawImage(bufCanvas, 0, 0);
+	}
+}
+// --------------------------------------------------------------------------------
+// remove a canvas
+CompositeCanvas.prototype.removeLayer = function(canvas) {
+	for (var i = 0, n = this.canvasLayer.length; i < n; i++) {
+	    if (this.canvasLayer[i] == canvas) {						
+		    // first copy canvas to buffer									
+			var curCanvas = document.getElementById( this.canvasLayer[i] ),
+			    curCtx = curCanvas.getContext('2d'),
+			    bufCanvas = document.getElementById('buffer'),
+			    bufCtx = bufCanvas.getContext('2d');									
+			// draw canvas to buffer
+			bufCtx.drawImage(curCanvas, 0, 0);									 						
+			this.canvasLayer.splice(i, 1); 	
+			this.elements.splice(i, 1);
+			var ele = document.getElementById(canvas);
+            ele.parentNode.removeChild(ele);
+		} 
+	}			
+}									
+// --------------------------------------------------------------------------------            						
+CompositeCanvas.prototype.canvasToBuffer = function(canvas) {								
+	var curCanvas = document.getElementById( canvas ),
+		bufCtx = this.getCtx('buffer');									
+	bufCtx.drawImage(curCanvas, 0, 0);
+	return true;
+}
+// --------------------------------------------------------------------------------			
+CompositeCanvas.prototype.bufferToCanvas = function(canvas) {						
+    var bufCanvas = document.getElementById('buffer'),				   
+	    curCtx = this.getCtx( canvas );									
+	curCtx.drawImage(bufCanvas, 0, 0);
+}
+// --------------------------------------------------------------------------------			
+CompositeCanvas.prototype.canvasToRender = function(canvas) {									
+	var curCanvas = document.getElementById( canvas ),
+		renCtx = this.getCtx('render');									
+	renCtx.drawImage(curCanvas, 0, 0);
+}
+// --------------------------------------------------------------------------------
+// helper for debugging
+CompositeCanvas.prototype.showLayer = function() {
+    console.log(this.canvasLayer);
+	console.log(this.elements);
+}
+
+
+
